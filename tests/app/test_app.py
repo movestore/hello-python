4fb0fd99071b3e02376516e1d80c9e0b4797558e
cleemansen <github-action@actions-template-sync.noreply.github.com>
2023-10-10 10:40:38 +0000

    chore(template): merge template changes :up:

--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -13,65 +13,13 @@ class MyTestCase(unittest.TestCase):
         os.environ['APP_ARTIFACTS_DIR'] = os.path.join(ROOT_DIR, 'tests/resources/output')
         self.sut = App(moveapps_io=MoveAppsIo())
 
-    def test_app_file_consume(self):
+    def test_app_returns_input(self):
         # prepare
-        os.environ['LOCAL_APP_FILES_DIR'] = os.path.join(ROOT_DIR, 'tests/resources/app/')
+        expected: mpd.TrajectoryCollection = pd.read_pickle(os.path.join(ROOT_DIR, 'tests/resources/app/input2.pickle'))
+        config: dict = {}
 
         # execute
-        actual = self.sut._consume_app_file()
+        actual = self.sut.execute(data=expected, config=config)
 
         # verif
-        self.assertEqual(11, actual)
-
-    def test_filter_output(self):
-        # prepare
-        testdata: mpd.TrajectoryCollection = pd.read_pickle(os.path.join(ROOT_DIR, 'tests/resources/app/app/input2.pickle'))
-        config = {
-            "individualLocalIdentifier": 742
-        }
-        self.assertEqual(3, len(testdata.trajectories))
-
-        # execute
-        actual = self.sut.execute(data=testdata, config=config)
-
-        # verify
-        self.assertEqual(1, len(actual.trajectories))
-        self.assertEqual(742, actual.trajectories[0].id)
-
-    def test_full_output_missing_key(self):
-        # prepare
-        testdata: mpd.TrajectoryCollection = pd.read_pickle(os.path.join(ROOT_DIR, 'tests/resources/app/app/input2.pickle'))
-        config = {}
-        self.assertEqual(3, len(testdata.trajectories))
-
-        # execute
-        actual = self.sut.execute(data=testdata, config=config)
-
-        # verify
-        self.assertEqual(3, len(actual.trajectories))
-
-    def test_full_output_null_key(self):
-        # prepare
-        testdata: mpd.TrajectoryCollection = pd.read_pickle(os.path.join(ROOT_DIR, 'tests/resources/app/app/input2.pickle'))
-        config = {
-            "individualLocalIdentifier": None
-        }
-        self.assertEqual(3, len(testdata.trajectories))
-
-        # execute
-        actual = self.sut.execute(data=testdata, config=config)
-
-        # verify
-        self.assertEqual(3, len(actual.trajectories))
-
-    def test_crash(self):
-        # prepare
-        testdata: mpd.TrajectoryCollection = pd.read_pickle(os.path.join(ROOT_DIR, 'tests/resources/app/app/input2.pickle'))
-
-        config = {
-            "forceCrash": True
-        }
-
-        # execute
-        with self.assertRaises(Exception):
-            self.sut.execute(data=testdata, config=config)
+        self.assertEqual(expected, actual)
